d2ea442805f7fe39c84b7c373fe2f83d4e87ba29
Chirag P <137880855+Chigich@users.noreply.github.com>
2025-11-24 15:40:12 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,12 +4,10 @@
 /* function prototypes */
 void biggest3(void);
 void factorial(void);
-void reverse(void);
 
 int main(void) {
     biggest3();
     factorial();
-    reverse();
     return 0;
 }
 
